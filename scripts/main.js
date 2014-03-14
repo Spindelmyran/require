10baecfeea318a5fb2d26c1218eee98d7ca0e55a
Tony Andersson <spindelmyran@gmail.com>
2014-03-14 10:38:25 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+require(["app"], function(app) {
+    //This function is called when scripts/alerter.js is loaded.
+    //If alerter.js calls define(), then this function is not fired until
+    //util's dependencies have loaded, and the util argument will hold
+    //the module value for "scripts".
+
+});
